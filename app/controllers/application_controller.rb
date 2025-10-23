3abb1e6018aa519f81d92b5c782c29ffbd6f1ed3
haramika <genemika0515@icloud.com>
2025-10-23 14:10:42 +0900

    kaminari課題提出

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   allow_browser versions: :modern
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,29 +3,25 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
-  # GET /books or /books.json
   def index
     @books = Book.all
+    @books = Book.page(params[:page]).per(3).order(:id)
   end
 
-  # GET /books/1 or /books/1.json
   def show; end
 
-  # GET /books/new
   def new
     @book = Book.new
   end
 
-  # GET /books/1/edit
   def edit; end
 
-  # POST /books or /books.json
   def create
     @book = Book.new(book_params)
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: t('controllers.common.notice_create', name: Book.model_name.human) }
+        format.html { redirect_to @book, notice: t(:create_message, scope: 'activerecord.success_messages') }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +30,10 @@ class BooksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human) }
+        format.html { redirect_to @book, notice: t(:update_message, scope: 'activerecord.success_messages') }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +42,21 @@ class BooksController < ApplicationController
     end
   end
 
-  # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy!
 
     respond_to do |format|
-      format.html { redirect_to books_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
+      format.html { redirect_to books_path, status: :see_other, notice: t(:destroy_message, scope: 'activerecord.success_messages') }
       format.json { head :no_content }
     end
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_book
     @book = Book.find(params.expect(:id))
   end
 
-  # Only allow a list of trusted parameters through.
   def book_params
     params.expect(book: %i[title memo author picture])
   end
